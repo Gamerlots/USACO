314a924646f9fbcd1d876a3c18529ea8bf937ba9
Gamerlots <aarindave@gmail.com>
2021-07-19 17:42:04 -0400

    Delete Promotion Counting directory

--- Promotion Counting/promote.py
+++
@@ -1,7 +0,0 @@
-with open("promote.in") as promotion_input:
-    DIVISIONS = [list(map(int, division.split())) for division in promotion_input][1:]
-    DIVISIONS = [after-before for before, after in DIVISIONS]
-    DIVISIONS = [str(sum(DIVISIONS[rank:])) for rank in range(3)]
-
-with open("promote.out", "w") as promotion_output:
-    promotion_output.write("\n".join(DIVISIONS))
